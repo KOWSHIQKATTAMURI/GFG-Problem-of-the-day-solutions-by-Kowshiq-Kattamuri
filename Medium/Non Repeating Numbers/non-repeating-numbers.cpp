bb8f3d032bedf5684c5ce50e4810774bbb8d2354
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-08-14 06:39:30 +0530

    Update non-repeating-numbers.cpp

--- Medium/Non Repeating Numbers/non-repeating-numbers.cpp
+++ Medium/Non Repeating Numbers/non-repeating-numbers.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Solution
 {
 public:
+    // Function to find the set bit.
     int getbitposition(int x){
         int pos = 0;
         while((x&1) != 1){
@@ -14,6 +15,16 @@ public:
         }
         return pos;
     }
+//Explanation: 
+	    // 1. The concept used here is that XOR of all those elements will be zero if their frequency is even.
+	    // 2. If the frequence is odd, then the XOR will result in that particular element
+	    // 3. Ex: In the array {1,1,2,3,3,4,4,5,5}, the XOR of all the elements will result in 2.
+	    // 4. Ex: In the array {1,1,2,3,3,4,4,5,5,6,6,6}, the XOR of all the elements will result in 2(XOR)6.
+	    // 5. So, take the XOR of all elements in the array and store the result in the variable named x.
+	    // 6. Find the position of any set bit in x. Let it be k.
+	    // 7. Take two elements int a = 0, b = 0. 
+	    // 8. If any particular element in the array has the sit bit at k, then take it XOR with a, else take it XOR with b.
+	    // 9. The final values of a, b will be the required result. 
     vector<int> singleNumber(vector<int> nums){
         int x = 0;
         for(int i = 0; i < nums.size(); i++){
