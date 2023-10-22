66890d3547a798a35d758a20c1ed5619688abacb
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-10-22 09:55:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of paths/number-of-paths.cpp
+++ Medium/Number of paths/number-of-paths.cpp
@@ -4,31 +4,30 @@ using namespace std;
  
 
 // } Driver Code Ends
-class Solution
-{
+class Solution{
     public:
     long long mod = 1e9 + 7;
-    long long inv(long long base, long long exponent){
+    long long binary_exponent(long long base, long long exponent){
         long long ans = 1;
         while(exponent > 0){
-            if(exponent%2 == 1){
+            if(exponent&1){
                 ans = (ans*base)%mod;
             }
             base = (base*base)%mod;
-            exponent /= 2;
+            exponent >>= 1;
         }
         return ans;
     }
     long long  numberOfPaths(int rows, int cols){
-        long long total_steps = rows + cols - 2;
-        long long min_steps = min(rows - 1, cols - 1);
+        long long n = rows + cols - 2;
+        long long r = min(rows - 1, cols - 1);
         long long numerator = 1, denominator = 1;
-        for(long long i = 0; i < min_steps; i++){
-            numerator = (numerator*(total_steps - i))%mod;
+        for(long long i = 0; i < r; i++){
+            numerator = (numerator*(n - i))%mod;
             denominator = (denominator*(i + 1))%mod;
         }
-        long long inverse_denominator = inv(denominator%mod, mod - 2);
-        return (numerator*inverse_denominator)%mod;
+        long long MMI_B = binary_exponent(denominator%mod, mod - 2);
+        return (numerator*MMI_B)%mod;
     }
 };
 
