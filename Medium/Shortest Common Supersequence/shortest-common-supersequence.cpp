fea052fb2bcef53b84684146e46a2e7f079d15aa
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-11-13 04:36:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution{
+    public:
+    int shortestCommonSupersequence(string X, string Y, int m, int n){
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(X[i - 1] == Y[j - 1]){
+                    curr[j] = 1 + prev[j - 1];
+                }else{
+                    curr[j] = max(curr[j - 1], prev[j]);
+                }
+            }
+            prev = curr;
+        }
+        int lcs = prev[n];
+        return n + m - lcs;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{   
+    
+    int t;
+    
+    //taking total testcases
+    cin >> t;
+    while(t--){
+    string X, Y;
+    //taking String X and Y
+	cin >> X >> Y;
+	
+	//calling function shortestCommonSupersequence()
+	Solution obj;
+	cout << obj.shortestCommonSupersequence(X, Y, X.size(), Y.size())<< endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
