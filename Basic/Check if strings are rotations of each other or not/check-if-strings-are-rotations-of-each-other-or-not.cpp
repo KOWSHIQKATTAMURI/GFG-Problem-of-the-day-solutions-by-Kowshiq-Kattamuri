17b5fb701fe5b51b1f4cf6c78ad50d694f1c9766
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-11-14 05:04:10 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check if strings are rotations of each other or not/check-if-strings-are-rotations-of-each-other-or-not.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool areRotations(string s1,string s2){
+        return s1.length() == s2.length() and (s1 + s1).find(s2) != string::npos;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
