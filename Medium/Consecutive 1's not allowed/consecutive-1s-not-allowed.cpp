f1db46ffb64314745c2c0c925045575377ab8f68
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-12-13 10:38:01 +0530

    Updated Solution - GfG to GitHub

--- Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
+++ Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
@@ -6,17 +6,18 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
+
 class Solution{
 public:
 	ll countStrings(int n){
 	    ll prev_zero = 1, prev_one = 1, curr_zero = 1, curr_one = 1, mod = 1e9 + 7;
-	    for(int i = 2; i <= n; i++){
+	    for(int length = 2; length <= n; length++){
 	        curr_zero = (prev_zero + prev_one)%mod;
-	        curr_one = prev_zero%mod;
+	        curr_one = (prev_zero)%mod;
 	        prev_zero = curr_zero;
 	        prev_one = curr_one;
 	    }
-	    return (curr_one + curr_zero)%mod;
+	    return (curr_zero + curr_one)%mod;
 	}
 };
 
